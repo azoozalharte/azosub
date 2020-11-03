5eaef3dec99fe420be3c593febcf2566e5570aad
azoozalharte <zezo50115@protonmail.com>
2020-11-03 20:30:31 +0300

    Azosub to subnetting a class C IP addresses

--- try.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        age = int(input("Enter your age: "))
-        if age > 0:
-            break
-        print("Invalid age entered")
-    except Exception as e:
-        print(e)
